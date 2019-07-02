e058f4cc5471627de9045df461d9d9ba0e0cda4a
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-02 00:48:05 -0700

    transferred login data to register

--- friend-finder/src/components/Login.js
+++ friend-finder/src/components/Login.js
@@ -1,85 +1,10 @@
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import Logo from '../images/LogoFF.png'
 
 const Login = props => {
-
-  const [values, setValues] = React.useState({
-    username: '',
-    email: '',
-    password: '',
-    phone: '',
-    state: '',
-    city: '',
-    bio: '',
-    firstName: '',
-    lastName: ''
-  })
-
-  const handleChange = prop => e => {
-    setValues({ ...values, [prop]: e.target.value });
-  };
-
-
   return (
-    <div className = "login-container">
-      <div className = 'login-top'> 
-        <img className = 'login-logo' src = {Logo} width = "45em" alt = "Logo"/> 
-        <h2> Login </h2>
-      </div>
-      <div className = "login-form">
-        <form>
-          <div className = 'login-form-inputs'> 
-          <input
-              value={values.username}
-              onChange={handleChange("username")}
-              placeholder = "username"
-          />
-          <input
-            value={values.firstName}
-            onChange={handleChange("firstName")}
-            placeholder = "First name"
-          />
-          <input
-            value={values.lastName}
-            onChange={handleChange("lastName")}
-            placeholder = "Last name"
-          />
-          <input
-              value={values.email}
-              onChange={handleChange("email")}
-              placeholder = "email"
-          />     
-          <input
-              value={values.password}
-              onChange={handleChange("password")}
-              placeholder = "password"
-              type = "password"
-          />     
-          <input
-              value={values.phone}
-              onChange={handleChange("phone")}
-              placeholder = "Phone Number"
-          />
-          <input
-              value={values.state}
-              onChange={handleChange("state")}
-              placeholder = "Enter State"
-          />
-          <input
-            value={values.city}
-            onChange={handleChange("city")}
-            placeholder = "Enter City"
-          />
-          <textarea
-            value={values.bio}
-            onChange={handleChange("bio")}
-            placeholder = "Enter a brief description of yourself"
-            cols="40" rows="5"
-          />
-          </div>
-        </form>
-      </div>
+    <div>
+      Login
     </div>
   )
 }
--- friend-finder/src/components/Signup.js
+++ friend-finder/src/components/Signup.js
@@ -1,10 +1,94 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Logo from '../images/LogoFF.png'
 
 const Signup = props => {
+
+  const [values, setValues] = React.useState({
+    username: '',
+    email: '',
+    password: '',
+    phone: '',
+    state: '',
+    city: '',
+    bio: '',
+    firstName: '',
+    lastName: ''
+  })
+
+  const handleChange = prop => e => {
+    setValues({ ...values, [prop]: e.target.value });
+  };
+
   return (
-    <div>
-      Signup
+    <div className = "login-container">
+      <div className = 'login-top'> 
+        <h1> Login </h1>
+        <img className = 'login-logo' src = {Logo} width = "45em" alt = "Logo"/> 
+
+      </div>
+      <div className = "login-form">
+        <form>
+          <div className = 'login-form-inputs'> 
+          <input
+              className = "input-styles"
+              value={values.username}
+              onChange={handleChange("username")}
+              placeholder = "username"
+          />
+          <input
+            className = "input-styles"
+            value={values.firstName}
+            onChange={handleChange("firstName")}
+            placeholder = "First name"
+          />
+          <input
+            className = "input-styles"
+            value={values.lastName}
+            onChange={handleChange("lastName")}
+            placeholder = "Last name"
+          />
+          <input
+              className = "input-styles"
+              value={values.email}
+              onChange={handleChange("email")}
+              placeholder = "email"
+          />     
+          <input
+              className = "input-styles"
+              value={values.password}
+              onChange={handleChange("password")}
+              placeholder = "password"
+              type = "password"
+          />     
+          <input
+              className = "input-styles"
+              value={values.phone}
+              onChange={handleChange("phone")}
+              placeholder = "Phone Number"
+          />
+          <input
+              className = "input-styles"
+              value={values.state}
+              onChange={handleChange("state")}
+              placeholder = "Enter State"
+          />
+          <input
+            className = "input-styles"
+            value={values.city}
+            onChange={handleChange("city")}
+            placeholder = "Enter City"
+          />
+          <textarea
+            className = "input-styles"
+            value={values.bio}
+            onChange={handleChange("bio")}
+            placeholder = "Enter a brief description of yourself"
+            cols="40" rows="5"
+          />
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
