70f50847d6cdf10c9fb632e4272eaa382ba48c3a
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-02 12:39:26 -0700

    landing page styled

--- friend-finder/src/components/Landing.js
+++ friend-finder/src/components/Landing.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Navbar from "./Navbar"
+import background from '../images/friendUnsplashed.jpg'
+import Logo from '../images/LogoFF.png'
 
 const Landing = props => {
   return (
     <div>
-      Landing
+  <div className = 'landingContainer'>
+    <img src = {background} className = 'landingImg' alt ='picnic on mountain'/>
+    <div className = 'landingText'>
+    <div className = 'textContainer'> 
+      <div className = 'innertext'>
+      <div className = 'Logo-landing'> 
+      <img src = {Logo} alt = 'app logo' className = 'landing-logo'/>
+        <h1 className = 'logo-title'> Friend Finder</h1>
+        </div>
+        <div className = 'landing-text'>
+        <h1> Meet your new best friend</h1>
+    
+      <div>
+        <p> Match with people that get your vibe, and meet awesome people.</p>
+      </div>
+      </div>
+      <div className = 'buttons-landing'> 
+        <button  className = 'cover this'  href = '/login'>Login</button>
+        <button  className = 'slide this' href = '/signup'>Sign Up</button>   
+      </div>
+      </div>
+      </div>
     </div>
+  </div>
+    
+  </div>
   )
 }
 
