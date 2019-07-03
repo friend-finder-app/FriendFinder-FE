7f61c2826170b898ceffa1f7aa284d85f18083df
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-02 22:17:51 -0700

    added hobbies on register

--- friend-finder/src/components/SideBar.js
+++ friend-finder/src/components/SideBar.js
@@ -6,13 +6,17 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { getUserinfo } from "../actions";
 import ImgUpload from './ImgUpload.jsx'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 class SideBar extends React.Component {
   constructor(props){
     super(props)
 
     this.state = {
-      profileImg: {}
+      profileImg: {},
+      secondSlide:{},
+      thirdSlide: {}
     }
   
   }
@@ -24,27 +28,42 @@ class SideBar extends React.Component {
       this.setState({profileImg: this.props.userInfo})
       // const profilePhoto = await this.props.userInfo.images[0].imageData
       this.setState({profileImg: this.props.userInfo.images[0].imageData})
-   
+      this.setState({secondSlide: this.props.userInfo.images[1].imageData})
+      this.setState({thirdSlide: this.props.userInfo.images[2].imageData})
  
   }
 
 
 
   render(){
-  //   if (!this.props.userInfo) {
-  //     return <span>Loading...</span>;
-  // }
+    if (!this.props.userInfo) {
+      return <span>Loading...</span>;
+  }
   return (
+
     <div className="side-bar">
       <div className="side-bar-img">
-
-        <img src= {`https://friendfinder-be.herokuapp.com/${this.state.profileImg}`} />     
- 
       <h2>Welcome {this.props.userInfo.firstName} </h2>
+      <Carousel>
+                <div>
+                <img src= {`https://friendfinder-be.herokuapp.com/${this.state.profileImg}`} />     
+ 
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                <img src= {`https://friendfinder-be.herokuapp.com/${this.state.secondSlide}`} />   
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                <img src= {`https://friendfinder-be.herokuapp.com/${this.state.thirdSlide}`} />   
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
         {/* <h3>Edit Profile</h3> */}
       </div>
       <ImgUpload/>
     </div>
+    
   )}
 };
 
