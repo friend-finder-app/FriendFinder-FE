efdd9688fe62f569005955d28f7e09aa7ff8a484
raberin <rmvaberin@gmail.com>
2019-07-02 19:01:03 -0700

    Created an ImgUpload component

--- friend-finder/src/components/SideBar.js
+++ friend-finder/src/components/SideBar.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Dummy from '../images/ProfileDummy.png'
-import Button from '../images/PlusButton.png'
+import React from "react";
+import PropTypes from "prop-types";
+import Dummy from "../images/ProfileDummy.png";
+import Button from "../images/PlusButton.png";
+import ImgUpload from "./ImgUpload";
 
 const SideBar = props => {
-
   return (
     <div className="side-bar">
       <div className="side-bar-img">
         <img src={Dummy} alt="user-image" />
         <h2>Welcome Test </h2>
         <h3>Edit Profile</h3>
+        <ImgUpload />
       </div>
     </div>
-  )
-}
+  );
+};
 
-SideBar.propTypes = {
+SideBar.propTypes = {};
 
-}
-
-export default SideBar
+export default SideBar;
