c2b8efdc4f4ee509b3cd426a2e6536ba126175be
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-24 22:30:00 -0700

    responsive landing page image/login

--- friend-finder/src/components/Landing.js
+++ friend-finder/src/components/Landing.js
@@ -12,27 +12,22 @@ const Landing = props => {
     <div>
   <div className = 'landingContainer'>
     <img src = {background} className = 'landingImg' alt ='picnic on mountain'/>
-    <div className = 'landingText'>
-    <div className = 'textContainer'> 
       <div className = 'innertext'>
-      <div className = 'Logo-landing'> 
-      <img src = {Logo} alt = 'app logo' className = 'landing-logo'/>
-        <h1 className = 'logo-title'> Friend Finder</h1>
+        <div className = 'Logo-landing'> 
+          <img src = {Logo} alt = 'app logo' className = 'landing-logo'/>
+          <h1 className = 'logo-title'> Friend Finder</h1>
         </div>
+        <div className= 'landing-description'> 
         <div className = 'landing-text'>
-        <h1> Meet your new best friend</h1>
-    
-      <div>
-        <p> Match with people that get your vibe, and meet awesome people.</p>
+          <h1> Meet your new best friend</h1>
+            <p> Match with people that get your vibe, and meet awesome people.</p>
+        </div>
+        <div className = 'buttons-landing'> 
+          <Link  className = 'slide this'  to = '/login'>Login</Link>
+          <Link  className = 'cover this' to = '/signup'>Sign Up</Link>   
+        </div>
+        </div>
       </div>
-      </div>
-      <div className = 'buttons-landing'> 
-        <Link  className = 'slide this'  to = '/login'>Login</Link>
-        <Link  className = 'cover this' to = '/signup'>Sign Up</Link>   
-      </div>
-      </div>
-      </div>
-    </div>
   </div>
     
   </div>
